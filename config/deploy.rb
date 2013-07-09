06d7ece2ba3e9e6da0df5ee09bdab71b8aa60351
Josh Kaplan <joshbrentkaplan@gmail.com>
2013-07-09 13:12:00 -0500

    Added cleanup to deploys

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,7 @@ role :web, "nextrips.josh-kaplan.com"                          # Your HTTP serve
 role :app, "nextrips.josh-kaplan.com"                          # This may be the same as your `Web` server
 
 # if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+after "deploy:restart", "deploy:cleanup"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
