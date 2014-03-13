b50570baa7b2d646c73920172100d498ead0d7b7
Josh Kaplan <joshbrentkaplan@gmail.com>
2014-03-12 19:48:22 -0500

    nextrips => flextrips

--- nextrips.rb
+++ nextrips.rb
@@ -5,12 +5,14 @@ require 'json'
 set :json_encoder, JSON
 
 
-def departures_for_stop(stop, desired_route = nil, real_time = nil)
+def departures_for_stop(stop, desired_routes = nil, real_time = nil)
   url = "http://www.metrotransit.org/Mobile/Nextrip.aspx?stopnumber=#{stop}"
 
   page = Nokogiri::HTML(open(url, read_timeout: 5))
 
   rows = page.css('.nextripDepartures .data')
+
+  desired_routes = [desired_routes] unless desired_routes.kind_of? Array
 
   departures = {nextrips:[]}
 
@@ -20,7 +22,7 @@ def departures_for_stop(stop, desired_route = nil, real_time = nil)
   rows.each do |row|
     departure = {}
     route = row.css('.col1').text
-    if ((desired_route && route.match(desired_route.to_s)) || !desired_route)
+    if ((desired_routes.length && desired_routes.any? { |desired_route| route.match desired_route.to_s}) || !desired_routes)
       departure[:route] = route
       departure[:desc] = row.css('.col2').text
       departure[:time] = row.css('.col3').text
@@ -38,20 +40,25 @@ end
 
 get '/nextrip/:stop' do
   real_time = params[:realTime].nil? ? false : params[:realTime] == "true"
-  departures = departures_for_stop(params[:stop], params[:route], real_time)
-  
+  departures = departures_for_stop(params[:stop], params[:routes].split(','), real_time)
+
   json departures
 end
 
 get '/commute' do
-  stops = [{stop: 1218, route: 18}, {stop: 53543, route: 535}, {stop: 51839, route: 535}, {stop: 1872, route: 18}]
+  stops = [
+    {stop: 1218, routes: ['18E', '18D']},
+    {stop: 48627, routes: [540]},
+    {stop: 51839, routes: [540]},
+    {stop: 1436, routes: [18]}
+  ]
 
   @nextrips = []
 
   real_time = params[:realTime].nil? ? false : params[:realTime] == "true"
 
   stops.each do |stop|
-    departures = departures_for_stop(stop[:stop], stop[:route], real_time)
+    departures = departures_for_stop(stop[:stop], stop[:routes], real_time)
     departures[:nextrips] = departures[:nextrips][0..2]
     @nextrips << departures
   end
